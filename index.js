c71d8079782fc1aa2cc2b79a020e2a035bc8f356
vbanurag <asharma.soccer@gmail.com>
2017-06-15 12:37:15 +0530

    array sort basic

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ var addon = require('./build/Release/myAddon.node');
 
 console.log(addon.sayHelloWorld());
 console.log(addon.add(3,5));
+console.log(addon.sortNode([8,1,5,25,4,85]));
--- myAddon.cc
+++ myAddon.cc
@@ -1,5 +1,6 @@
 #include <nan.h>
 #include <iostream>
+#include <algorithm>
 using namespace std;
 using v8::FunctionCallbackInfo;
    using v8::Isolate;
@@ -12,6 +13,7 @@ using v8::FunctionCallbackInfo;
    using v8::HandleScope;
    using v8::Handle;
    using v8::Exception;
+   using v8::Array;
 
 
    /**
@@ -45,12 +47,36 @@ using v8::FunctionCallbackInfo;
 
    }
 
+   // Array sort using template vector sort method
+
+   void arraySort(const FunctionCallbackInfo<Value> &args){
+    Isolate* isolate = Isolate::GetCurrent();
+    HandleScope scope(isolate);
+    int n = args[0]->NumberValue();
+    //Local<Array> arr = Array::New(isolate, n);
+
+    Local<Array> input_array = Local<Array>::Cast(args[0]);
+    int temp;
+
+    for(int i =0 ; i< input_array->Length(); i++){
+        for(int j =0 ; j<input_array->Length()-i-1; j++){
+            if(input_array->Get(j)->NumberValue()>input_array->Get(j+1)->NumberValue()){
+                temp = input_array->Get(j)->NumberValue();
+                input_array->Set(j,Number::New(isolate,input_array->Get(j+1)->NumberValue()));
+                input_array->Set(j+1, Number::New(isolate,temp));
+            }
+        }
+        //arr->Set(i, Number::New(isolate, input_array->Get(i)->NumberValue()));
+    }
+    args.GetReturnValue().Set(input_array);
+   }
 
 /*entry point,we can recieve upto two arguments here.First is export like  module.export
 */
 void Init(Handle<Object> exports) {
   NODE_SET_METHOD(exports, "sayHelloWorld", sayHelloWorld);//NODE_SET_MET  HOD to export
   NODE_SET_METHOD(exports, "add", addTwoValue);
+  NODE_SET_METHOD(exports,"sortNode",arraySort);
 }
 
 //to define entry point,first argument must match with target name in binding.gyp
